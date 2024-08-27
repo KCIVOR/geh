55acc5280f0944dac3290cb9d177e91b36fbf40a
KCIVOR <rvckmlnrmsnt@gmail.com>
2024-08-27 18:41:45 +0800

    test

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
 
 public class test {
     public static void main(String args[]) {
-      int x=10;
-      int y=25;
-      int z=x+y;
-      int x=10;
-      int y=25;
-      int z=x+y;
+      
       System.out.println("Sum of x+y = " + z);
       yown
     }
